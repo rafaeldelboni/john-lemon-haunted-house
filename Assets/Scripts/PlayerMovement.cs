0ff11323a8993cf31ef9764d92d3f9156e8eeed3
Rafael Delboni <rafadelboni@gmail.com>
2023-11-27 21:54:45 -0300

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Vector3 m_Movement;
+    Quaternion m_Rotation;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalMovement = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalMovement = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalMovement || hasVerticalMovement;
+        m_Animator.SetBool("IsWalking", isWalking);
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# John Lemon Haunted House - Unity Tutorial
+My result of following Unity's official tutorial [John Lemon's Haunted Jaunt: 3D Beginner](https://learn.unity.com/project/john-lemon-s-haunted-jaunt-3d-beginner?uv=2020.3)
+
+I used Unity version 2022.3.13f1 with URP.
+
+## Licenses
+All the [assets](https://assetstore.unity.com/packages/essentials/tutorial-projects/unity-learn-3d-beginner-tutorial-resources-urp-143848) are under [Standard Unity Asset Store EULA](https://unity.com/legal/as-terms)
